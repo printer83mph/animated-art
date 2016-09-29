cd6d7d296ec3b67c76b41602c57d3799987e924e
printer83mph <printer.83mph@gmail.com>
2016-09-29 18:23:37 -0400

    Make art scalable

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 var things = [];
 var change;
+var squareSize;
+var waveAmount;
 var other = false;
 var row = 0;
 
@@ -8,10 +10,10 @@ function ThingOne(xPos,yPos) {
   this.draw = function() {
     beginShape();
     vertex(this.position.x,this.position.y); // begin shape at top left
-    quadraticVertex(this.position.x+15,this.position.y-change,this.position.x+30,this.position.y); // to top right
-    quadraticVertex(this.position.x+30-change,this.position.y+15,this.position.x+30,this.position.y+30); // to bottom right
-    quadraticVertex(this.position.x+15,this.position.y+30+change,this.position.x,this.position.y+30); // to bottom left
-    quadraticVertex(this.position.x+change,this.position.y+15,this.position.x,this.position.y); // to top left
+    quadraticVertex(this.position.x+squareSize/2,this.position.y-change,this.position.x+squareSize,this.position.y); // to top right
+    quadraticVertex(this.position.x+squareSize-change,this.position.y+squareSize/2,this.position.x+squareSize,this.position.y+squareSize); // to bottom right
+    quadraticVertex(this.position.x+squareSize/2,this.position.y+squareSize+change,this.position.x,this.position.y+squareSize); // to bottom left
+    quadraticVertex(this.position.x+change,this.position.y+squareSize/2,this.position.x,this.position.y); // to top left
     endShape();
   }
 }
@@ -21,10 +23,10 @@ function ThingTwo(xPos,yPos) {
   this.draw = function() {
     beginShape();
     vertex(this.position.x,this.position.y); // begin shape at top left
-    quadraticVertex(this.position.x+15,this.position.y+change,this.position.x+30,this.position.y); // to top right
-    quadraticVertex(this.position.x+30+change,this.position.y+15,this.position.x+30,this.position.y+30); // to bottom right
-    quadraticVertex(this.position.x+15,this.position.y+30-change,this.position.x,this.position.y+30); // to bottom left
-    quadraticVertex(this.position.x-change,this.position.y+15,this.position.x,this.position.y); // to top left
+    quadraticVertex(this.position.x+squareSize/2,this.position.y+change,this.position.x+squareSize,this.position.y); // to top right
+    quadraticVertex(this.position.x+squareSize+change,this.position.y+squareSize/2,this.position.x+squareSize,this.position.y+squareSize); // to bottom right
+    quadraticVertex(this.position.x+squareSize/2,this.position.y+squareSize-change,this.position.x,this.position.y+squareSize); // to bottom left
+    quadraticVertex(this.position.x-change,this.position.y+squareSize/2,this.position.x,this.position.y); // to top left
     endShape();
   }
 }
@@ -33,17 +35,19 @@ function setup() {
   canvas = createCanvas(window.innerWidth,window.innerHeight);
   fill(0,0);
   stroke(0);
-  strokeWeight(1);
 	smallestScr = min(width,height);
-	console.log(smallestScr);
-  for(var x = (width-smallestScr/2)/2; x < width-(width-smallestScr/2)/2 - 30; x += 37) {
+	squareSize = smallestScr*0.042;
+	waveAmount = smallestScr*0.01;
+	// console.log(smallestScr);
+	strokeWeight(smallestScr/500);
+  for(var x = (width-smallestScr/2)/2 + smallestScr * 0.008; x < width-(width-smallestScr/2)/2; x += smallestScr/20) {
 		console.log('x: ' + x);
 		if(row % 2 === 0) {
 			other = false;
 		} else {
 			other = true;
 		}
-    for(var y = (height-smallestScr/2)/2; y < height-(height-smallestScr/2)/2 - 30; y += 37) {
+    for(var y = (height-smallestScr/2)/2 + smallestScr * 0.008; y < height-(height-smallestScr/2)/2; y += smallestScr/20) {
 			console.log('y: ' + y);
       if(other) {
         things.push(new ThingOne(x,y));
@@ -59,7 +63,7 @@ function setup() {
 
 function draw() {
   background(255);
-	change = sin(frameCount/50)*10;
+	change = sin(frameCount/50)*waveAmount;
   for(var i = 0; i < things.length; i ++) {
     things[i].draw();
   }
